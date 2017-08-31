1921863104d014c3fcfd080674bba0f58832bc1d
PriyankaKalke <31509542+PriyankaKalke@users.noreply.github.com>
2017-08-31 18:01:29 +0530

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Title="Home Page" Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TypeWriter._Default" %>
+
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $("button").click(function () {
+            $("#test").hide();
+        });
+    });
+</script>
+</head>
+
+    
+    <head>
+        <script type = "text/javascript" src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+      <script type = "text/javascript">
+          $(document).ready(function () {
+              $("#btnCompare").click(function () {
+                  $("p").hide();
+              });
+          });
+      </script>         
+    </head>
+    <body>
+        <form id="form1" runat="server">
+        <h3>Type Writer</h3>
+        <table>
+            <tr>
+                <td>
+                    <div>
+                        <asp:TextBox ID="txtInput" runat="server" Height="200px" Width="400px" Wrap="true" TextMode="MultiLine"></asp:TextBox>
+                        
+                    </div>
+                </td>
+                <td>
+                    <div>
+                        <asp:TextBox ID="txtOutput" runat="server" Height="200px" Width="400px" Wrap="true" TextMode="MultiLine" ></asp:TextBox>
+
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                <asp:Label ID="lbltext" runat="server"></asp:Label>
+                <td colspan="2">
+            </tr>
+            <tr>
+                <td colspan="2">                
+                    <asp:Button ID="btnCompare" runat="server"  Text="Compare" OnClick="btnCompare_Click" />
+                </td>
+            </tr>
+        </table>
+
+        </form>
+
+    </body>
+        
+</html>
+
+
+
---
+++ Default.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TypeWriter
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        
+        protected void btnCompare_Click(object sender, EventArgs e)
+        {
+                TypewriterLogic tl = new TypewriterLogic();
+                string[] input = txtInput.Text.Split(' ').ToArray();
+                string[] output = txtOutput.Text.Split(' ').ToArray();
+
+            List<KeyValuePair<string, int>> Difference = new List<KeyValuePair<string, int>>();
+
+            Difference = tl.CompareData(input, output);
+
+            foreach(var item in Difference)
+            {
+                string str = (item.Key == "" || item.Key == null) ? " " : item.Key;
+                output[item.Value] = "<mark >" + str + "</mark>";
+            }
+
+            lbltext.Text = String.Join(" ", output);
+            }
+        }
+
+    public class TypewriterLogic
+    {
+        public List<KeyValuePair<string, int>> CompareData(string[] input, string[] output)
+        {
+            List<string> expected = new List<string>();
+            List<KeyValuePair<string, int>> diff = new List<KeyValuePair<string, int>>();
+            int size = 0;
+            if (input.Length >= output.Length)
+            {
+                size = output.Length;
+            }
+            else
+            {
+                size = input.Length;
+                for (int i = size; i < output.Length; i++)
+                {
+                    diff.Add(new KeyValuePair<string, int>(output[i], i));
+                }
+            }
+
+            for (int j = 0; j < size; j++)
+            {
+                expected.Add(input[j]);
+                if (expected.Contains(output[j]))
+                {
+                    expected.Remove(output[j]);
+                }
+                else
+                {
+                    diff.Add(new KeyValuePair<string, int>(output[j], j));
+                }
+            }
+            //diff.AddRange(expected);            
+            return diff;
+
+        }
+    }
+}
